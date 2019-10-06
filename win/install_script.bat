d840fc020972fcafaac4e3ef16b1778517a3a202
Joshua <joshuanazareth97@gmail.com>
2019-10-06 13:39:56 +0530

    added install script for important windows porgrams

---
+++ win/install_script.bat
@@ -0,0 +1,18 @@
+
+
+::Install Commands for Chocolatey
+
+choco install 7zip
+choco install atom
+choco install deluge
+choco install dropbox
+choco install everything
+choco install google-backup-and-sync
+choco install handbrake
+choco install notepadplusplus
+choco install nvm
+choco install python
+choco install spotify
+choco install vlc
+choco install vscode
+choco install wiztree
